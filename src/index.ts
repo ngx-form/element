c23e632730b431aea4d8c13ebb5bcaf61c302992
Diabeu <scibor.rudnicki@ster-project.pl>
2017-04-27 19:13:57 +0200

    refactor(index): export FormElementComponent

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-
+import { FormElementComponent } from './ngx-form-element.component';
 import { FormElementModule } from './ngx-form-element.module';
 
 export {
+  FormElementComponent,
   FormElementModule,
 }
 
