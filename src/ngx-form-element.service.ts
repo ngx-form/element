efc0fdf0408a30ea85d4b44882992704c38da2f9
Diabeu <scibor.rudnicki@ster-project.pl>
2017-06-05 18:55:59 +0200

    refactor(FormElementService): change import, add code description, change interfaces,  change method element

--- src/ngx-form-element.service.ts
+++ src/ngx-form-element.service.ts
@@ -4,17 +4,33 @@ import { Inject, Injectable, Optional, Type } from '@angular/core';
 // internal
 import { component } from '@ngx-core/common';
 import { element } from '@ngx-form/type';
-import { FormElementConfigInterface, FormComponentInterface } from '@ngx-form/interface';
+import { FormElementConfigInterface, ConfigElementInterface } from '@ngx-form/interface';
 
+/**
+ * forRoot config with possible elements to create.
+ * @export
+ * @class FormElementConfig
+ * @implements {FormElementConfigInterface}
+ */
 export class FormElementConfig implements FormElementConfigInterface {
-  types: Array<FormComponentInterface>;
+  elements: Array<ConfigElementInterface>;
 };
 
+/**
+ * Service to hold the config with configured elements component
+ * @export
+ * @class FormElementService
+ */
 @Injectable()
 export class FormElementService {
 
   private _config: FormElementConfig;
 
+  /**
+   * Creates an instance of FormElementService.
+   * @param {FormElementConfig} config
+   * @memberof FormElementService
+   */
   constructor( @Optional() config: FormElementConfig) {
     if (config) {
       this.config = config;
@@ -23,16 +39,17 @@ export class FormElementService {
   }
 
   /**
-   * Search element name in config types and return component assigned to it
-   * @param element name of html element for example 'input'
+   * Search element `name` in config `elements` and return component assigned to it.
+   * @param {element} element name of html element for example 'input'
+   * @returns {null | any}
    */
-  element(element: element): any {
+  find(element: element): null | any {
     let t: component | null = null;
     if (element) {
       if (this.config) {
-        if (this.config.types instanceof Array && this.config.types.length > 0) {
-          this.config.types.forEach((value, index) => {
-            if (value['type'] === element) {
+        if (this.config.elements instanceof Array && this.config.elements.length > 0) {
+          this.config.elements.forEach((value, index) => {
+            if (value['name'] === element) {
               t = value.component;
             }
           });
@@ -45,7 +62,7 @@ export class FormElementService {
   set config(config: FormElementConfig) {
     this._config = config;
   }
-  get config() {
+  get config(): FormElementConfig {
     return this._config;
   }
 }
