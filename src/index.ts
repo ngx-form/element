99b11846d3bf6239705b40341c410741e6a6e47d
Diabeu <scibor.rudnicki@ster-project.pl>
2017-07-18 16:42:55 +0200

    refactor: export ValidatorService

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { FormElementComponent } from './ngx-form-element.component';
 import { FormElementModule } from './ngx-form-element.module';
 import { FormElementConfig } from './ngx-form-element.service';
+import { ValidatorService } from './ngx-form-element-validator.service';
 
 export {
   FormElementConfig,
   FormElementComponent,
-  FormElementModule
+  FormElementModule,
+  ValidatorService
 }
 
 export default FormElementModule;
