1bae9c11480d84b23c3b7b4ce2f149f71170b911
Diabeu <scibor.rudnicki@ster-project.pl>
2017-07-18 16:43:43 +0200

    test: add config to tests

---
+++ test/config.ts
@@ -0,0 +1,25 @@
+
+export const config: ConfigElementInterface = {
+  attributes: {
+    autocomplete: 'off',
+
+    // Material Design
+    color: 'accent',
+    floatPlaceholder: 'never',
+    hideRequiredMarker: true,
+    // end
+
+    disabled: false,
+    placeholder: 'Firstname',
+    required: false,
+    type: 'text'
+  },
+  destroy: {
+    onCancelled: false,
+    onChanged: false,
+    onSubmitted: false
+  },
+  element: 'input',
+  focus: true,
+  key: 'firstname'
+};
