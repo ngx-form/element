78814fc630dd7dcebda9777bdc64b45be493f1be
Diabeu <scibor.rudnicki@ster-project.pl>
2017-07-18 18:20:59 +0200

    fix: export FormElementService

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { FormElementComponent } from './ngx-form-element.component';
 import { FormElementModule } from './ngx-form-element.module';
-import { FormElementConfig } from './ngx-form-element.service';
+import { FormElementConfig, FormElementService } from './ngx-form-element.service';
 import { ValidatorService } from './ngx-form-element-validator.service';
 
 export {
   FormElementConfig,
   FormElementComponent,
   FormElementModule,
-  ValidatorService
+  FormElementService,
+  ValidatorService,
 }
 
 export default FormElementModule;
