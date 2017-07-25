87d5919045bfa5eeeb21fd5ef7e451f7a9244ec4
Diabeu <scibor.rudnicki@ster-project.pl>
2017-07-25 16:28:43 +0200

    refactor: simplify index

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,5 @@
-import { FormElementComponent } from './ngx-form-element.component';
-import { FormElementModule } from './ngx-form-element.module';
-import { FormElementConfig, FormElementService } from './ngx-form-element.service';
-import { ValidatorService } from './ngx-form-element-validator.service';
-
-export {
-  FormElementConfig,
-  FormElementComponent,
-  FormElementModule,
-  FormElementService,
-  ValidatorService,
-}
-
-export default FormElementModule;
+export { ErrorService } from './error.service';
+export { FormElementComponent } from './ngx-form-element.component';
+export { FormElementModule } from './ngx-form-element.module';
+export { FormElementConfig, FormElementService } from './ngx-form-element.service';
+export { ValidatorService } from './validator.service';
